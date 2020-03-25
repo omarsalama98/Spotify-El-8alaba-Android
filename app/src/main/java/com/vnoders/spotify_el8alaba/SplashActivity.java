122cdf8b9fc2b5227c583f2019a5ec043d3efa99
Omar Mostafa <3omar.7afez2022@gmail.com>
2020-03-25 04:08:29 +0200

    Improved Splash Screen Speed (Opens Immediately)

--- app/src/main/java/com/vnoders/spotify_el8alaba/SplashActivity.java
+++ app/src/main/java/com/vnoders/spotify_el8alaba/SplashActivity.java
@@ -17,7 +17,7 @@ public class SplashActivity extends AppCompatActivity {
         final Runnable runnable = new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                Intent intent = new Intent(SplashActivity.this, firstScreen.class);
                 startActivity(intent);
             }
         };
