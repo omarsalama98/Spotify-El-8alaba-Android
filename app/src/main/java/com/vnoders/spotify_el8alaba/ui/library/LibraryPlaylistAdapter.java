d7857b3b7cd2b913f884b1b3807ebf501285331e
Omar Mostafa <3omar.7afez2022@gmail.com>
2020-04-22 08:24:01 +0200

    Hotfix to previous commit (forgot to include Toast package)

--- app/src/main/java/com/vnoders/spotify_el8alaba/ui/library/LibraryPlaylistAdapter.java
+++ app/src/main/java/com/vnoders/spotify_el8alaba/ui/library/LibraryPlaylistAdapter.java
@@ -8,6 +8,7 @@ import android.view.View.OnLongClickListener;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
