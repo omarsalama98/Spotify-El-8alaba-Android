9e12b9517fb5d5f9708aa1b205e30a9721e6e995
MohamedSamiMohamed <Mohamed.samy9956@gmail.com>
2020-06-08 22:33:44 +0200

    change password validation in sign up.

--- app/src/main/java/com/vnoders/spotify_el8alaba/ui/signup/SignUpPassword.java
+++ app/src/main/java/com/vnoders/spotify_el8alaba/ui/signup/SignUpPassword.java
@@ -63,7 +63,7 @@ public class SignUpPassword extends Fragment {
     private boolean isValidPassword(String password) {
         Pattern pattern;
         Matcher matcher;
-        final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{4,}$";
+        final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[A-Z])(?=\\S+$).{4,}$";
         pattern = Pattern.compile(PASSWORD_PATTERN);
         matcher = pattern.matcher(password);
         return matcher.matches();
